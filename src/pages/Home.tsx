b7d97d4aab907db0879be631762233d4ebc3886d
Júlia Gomes <gomesjulia621@gmail.com>
2022-06-11 18:01:59 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+
+export function Home() {
+  return (
+    <div>
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Toda pergunta tem uma resposta.</strong>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+      </aside>
+      <main>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </main>
+    </div>
+  )
+}
