c6bed01ebb5c502f052d48e367bbc69a98ab84b5
Júlia Gomes <gomesjulia621@gmail.com>
2022-06-11 20:52:57 -0300

    finished: home application version #01

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
+import googleImg from '../assets/images/google-icon.svg'
+
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
 
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img
           src={illustrationImg}
@@ -12,9 +16,22 @@ export function Home() {
         <strong>Toda pergunta tem uma resposta.</strong>
         <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
       </aside>
+
       <main>
-        <div>
-          <img src="" alt="" />
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+
+          <button className="create-room">
+            <img src={googleImg} alt="Logo do Google" />
+            Crie a sua sala com o Google
+          </button>
+
+          <div className="separator">Ou entre em uma sala </div>
+
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
         </div>
       </main>
     </div>
